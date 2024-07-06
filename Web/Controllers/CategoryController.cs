f3d89f962c3d88fae5bb6bffe7c3152aaa0a7ee9
DimaPalamarchuk <dimapalamarchuk310@gmail.com>
2024-07-06 18:16:07 +0200

    Add TempDarta

--- Web/Controllers/CategoryController.cs
+++ Web/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ namespace Web.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["Success"] = "Category created successfully!";
                 return RedirectToAction("Index");
             }
             return View();
@@ -62,6 +63,7 @@ namespace Web.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["Success"] = "Category updated successfully!";
                 return RedirectToAction("Index");
             }
             return View();
@@ -95,6 +97,7 @@ namespace Web.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["Success"] = "Category deleted successfully!";
             return RedirectToAction("Index");
         }
     }
